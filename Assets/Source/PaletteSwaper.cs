a9097253ec76a34120f986153650429b75a2b7c9
Little coding wolf <nicholasgennady@outlook.com>
2018-01-18 23:11:55 -0600

    Version 1.0
    -Palette Swapping with Fragment/Vertex Shader
    -Can be used with objects with Sprite Renderers or put onto the Camera to affect All objects.

---
+++ Assets/Source/PaletteSwaper.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PaletteSwaper : MonoBehaviour
+{
+    public Color[] colorPallet;
+    private Material material;
+    private SpriteRenderer spriteRenderer;
+    public Shader shader;
+
+    [ExecuteInEditMode]
+    private void Start()
+    {
+        if (shader != null)
+            material = new Material(shader);
+    }
+
+    //Used For Individual objects
+    [ExecuteInEditMode]
+    private void Update()
+    {
+        if (shader == null)
+            return;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.material = material;
+            material.SetColorArray("_Out", colorPallet);
+        }
+    }
+
+    //Used On Camera object to effect all objects
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (shader == null)
+            return;
+            
+        material.SetColorArray("_Out", colorPallet);
+        Graphics.Blit(source, destination, material);
+    }
+}
---
+++ Assets/Source/Shaders/Shader_PaletteSwap.shader
@@ -0,0 +1,46 @@
+﻿Shader "Custome/PaletteSwapArray"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+
+	SubShader
+	{
+        Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#include "UnityCG.cginc"
+			#pragma vertex vert
+			#pragma fragment frag
+
+			sampler2D _MainTex;
+			half4 _Out[256];
+			
+			struct v2f
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			v2f vert (v2f v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			half4 frag (v2f i) : SV_Target
+			{
+			   half4 c = tex2D(_MainTex, i.uv);
+			   if (c.r == c.b && c.r == c.g && c.a != 0)
+			       return _Out[ c.r * 300];	
+			return c;
+			}
+			ENDCG
+		}
+	}
+}
